ec705d89f3cce0e81e86fb180f3a22d57567d8e1
Emmanuel Gautier <git@emmanuelgautier.fr>
2018-04-01 16:15:53 +0200

    redesign website with foundation

---
+++ src/assets/js/app.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+
+// import Foundation from 'foundation-sites';
+import './lib/foundation-explicit-pieces';
+
+
+$(document).foundation();
