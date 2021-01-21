2395572c420fc89467937c95c5e039132083c037
Emmanuel Gautier <git@emmanuelgautier.fr>
2021-01-22 00:59:50 +0100

    feat: add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import SEO from '../next-seo.config'
 import { GTM_ID } from '../lib/gtm'
 
 interface Props {
@@ -8,9 +9,38 @@ interface Props {
 
 export default class MyDocument extends Document<Props> {
   render() {
+    const { locale = process.env.DEFAULT_LOCALE } = this.props
+
     return (
-      <Html lang={this.props.locale}>
+      <Html lang={locale}>
         <Head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="HandheldFriendly" content="True" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href={`https://${SEO.siteUrl}/apple-touch-icon.png`}
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href={`https://${SEO.siteUrl}/favicon-32x32.png`}
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href={`https://${SEO.siteUrl}/favicon-16x16.png`}
+            sizes="16x16"
+          />
+
+          <link
+            rel="manifest"
+            href={`https://${SEO.siteUrl}/site.webmanifest`}
+          />
+
           {/* Google Tag Manager - Global base code */}
           <script
             dangerouslySetInnerHTML={{
