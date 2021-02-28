808538a76271f604ba20c8215c9c3debf36fbd4f
Emmanuel Gautier <git@emmanuelgautier.fr>
2021-02-28 19:33:27 +0100

    feat: fix i18n _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,5 @@
-import '@fontsource/open-sans'
-import '../styles/globals.css'
-import '../styles/prism.css'
-
 import { useAmp } from 'next/amp'
-import { AppProps } from 'next/app'
+import App, { AppProps } from 'next/app'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { DefaultSeo } from 'next-seo'
@@ -12,6 +8,10 @@ import { IntlConfig, IntlProvider } from 'react-intl'
 import { polyfill } from '../polyfills'
 
 import SEO from '../next-seo.config'
+
+import '@fontsource/open-sans'
+import '../styles/globals.css'
+import '../styles/prism.css'
 
 type Props = Pick<IntlConfig, 'messages' | 'locale'> & AppProps
 
@@ -77,38 +77,52 @@ function MyApp({ Component, pageProps, messages, locale }: Props) {
  * @returns {[string, Promise]} A tuple containing the negotiated locale
  * and the promise of fetching the translated messages
  */
-function getMessages(locale: string) {
-  let langBundle
-  switch (locale) {
-    case 'en':
-      langBundle = import('../compiled-lang/en.json')
-      break
-    case 'fr':
-    default:
-      langBundle = import('../compiled-lang/fr.json')
-      break
+function getMessages(locales: string | string[] = ['en']) {
+  if (!Array.isArray(locales)) {
+    locales = [locales]
   }
-
-  return langBundle
+  let langBundle
+  let locale
+  for (let i = 0; i < locales.length && !locale; i++) {
+    locale = locales[i]
+    switch (locale) {
+      case 'en':
+        langBundle = import('../compiled-lang/en.json')
+        break
+      case 'fr':
+        langBundle = import('../compiled-lang/fr.json')
+        break
+      default:
+        break
+    }
+  }
+  if (!langBundle) {
+    return ['en', import('../compiled-lang/en.json')]
+  }
+  return [locale, langBundle]
 }
 
 function getLocalizedUrl(url: string, basePath: string, path: string) {
   return `${url}${basePath}${path}`
 }
 
-MyApp.getStaticProps = async ({
-  locale: nextLocale,
-  params: { locale: localeParam },
-}: any) => {
-  const locale = localeParam || nextLocale || process.env.DEFAULT_LOCALE
-  const messagePromise = getMessages(locale)
+const getInitialProps: typeof App.getInitialProps = async appContext => {
+  const locale = appContext.router.locale || process.env.NEXT_LOCALE
+  const [supportedLocale, messagePromise] = getMessages(locale)
 
-  const [, messages] = await Promise.all([polyfill(locale), messagePromise])
+  const [, messages, appProps] = await Promise.all([
+    polyfill(supportedLocale),
+    messagePromise,
+    App.getInitialProps(appContext),
+  ])
 
   return {
-    locale,
+    ...(appProps as any),
+    locale: supportedLocale,
     messages: messages.default,
   }
 }
+
+MyApp.getInitialProps = getInitialProps
 
 export default MyApp
