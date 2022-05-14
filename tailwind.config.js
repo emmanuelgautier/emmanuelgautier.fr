3d6bb6e770cddee21d78970a31a245bdc994a4e3
Emmanuel Gautier <git@emmanuelgautier.fr>
2022-05-14 16:51:58 +0200

    build: clean old tailwindcss configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 const { spacing, fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
-  mode: 'jit',
+  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: 'class',
   theme: {
     extend: {
@@ -72,9 +71,5 @@ module.exports = {
       }),
     },
   },
-  variants: {
-    typography: ['dark'],
-  },
-
   plugins: [require('@tailwindcss/typography')],
 }
