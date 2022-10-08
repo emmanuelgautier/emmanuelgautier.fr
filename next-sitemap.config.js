839be78ee3ed55b9305fdae1f3dc98aa4772cac8
Emmanuel Gautier <git@emmanuelgautier.fr>
2022-10-08 15:42:25 +0200

    feat: disallow cloudflare cdn-cgi for crawlers

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,6 +5,13 @@ const { siteUrl, i18n } = require('./next-seo.config')
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{
+      userAgent: '*',
+      allow: '/',
+      disallow: '/cdn-cgi/'
+    }]
+  },
   alternateRefs: i18n.domains.map(({ domain, defaultLocale }) => ({
     href: domain,
     hreflang: defaultLocale,
